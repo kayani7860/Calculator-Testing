1591074b35a716297172c86ece5180e19ef3f1c5
Hammad Kayani <36533976+kayani7860@users.noreply.github.com>
2021-03-12 12:48:51 +0100

    Update Calculator_functions.java
    Multiplication and Division Function added to the working branch

--- app/src/main/java/com/calculator/Calculator_functions.java
+++ app/src/main/java/com/calculator/Calculator_functions.java
@@ -1,4 +1,20 @@
 package com.calculator;
 
 public class Calculator_functions {
+
+    public static double multiplication(double number1, double number2)
+    {
+        return number1 - number2;
+    }
+
+
+    public static double division(double number1, double number2)
+    {
+        double result = number1 / number2;
+
+        if(result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY)
+            throw new ArithmeticException("Cannot divide by zero.");
+        else
+            return result;
+    }
 }
