8480897dcd985511b0635492e962f9aff967389c
Sai Priya Taniparthi <saipriya24194@gmail.com>
2021-03-14 20:34:27 +0100

    Update CalculatorTest.java

--- app/src/test/java/com/bth/calculator/CalculatorTest.java
+++ app/src/test/java/com/bth/calculator/CalculatorTest.java
@@ -9,8 +9,34 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest
 {
     private static final double DELTA = 0.001;
+@Test
+public void testSum() throws Exception
+{
+double number1 = 2.5;
+double number2 = 1.3;
+double expected = 3.8;
 
+double output = Calculator.sum(number1, number2);
+assertEquals(expected, output, DELTA);
+}
 
+@Test
+public void testMinus() throws Exception
+{
+double number1 = 2.5;
+double number2 = 1.3;
+double expected = 1.2;
+
+double output = Calculator.minus(number1, number2);
+assertEquals(expected, output, DELTA);
+
+number1 = 1.0;
+number2 = 2.5;
+expected = -1.5;
+output = Calculator.minus(number1, number2);
+assertEquals(expected, output, DELTA);
+}
+ 
     @Test
     public void testMultiplication() throws Exception
     {
