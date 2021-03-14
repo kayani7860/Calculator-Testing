205e3610174a33431b3846827811724b4a475ce4
Sai Priya Taniparthi <saipriya24194@gmail.com>
2021-03-14 20:31:04 +0100

    Update Calculator.java

--- app/src/main/java/com/bth/calculator/models/Calculator.java
+++ app/src/main/java/com/bth/calculator/models/Calculator.java
@@ -2,6 +2,16 @@ package com.bth.calculator.models;
 
 public class Calculator
 {
+	public static double sum(double number1, double number2)
+{
+return number1 + number2;
+}
+
+
+public static double minus(double number1, double number2)
+{
+return number1 - number2;
+}
 
     public static double multiplication(double number1, double number2)
     {
